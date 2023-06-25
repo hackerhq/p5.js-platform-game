db594ff6315c03de4889a394dee09acd72a9c426
hackerhq <137334394+hackerhq@users.noreply.github.com>
2023-06-25 17:14:23 -0400

    Add files via upload

---
+++ game2.js
@@ -0,0 +1,3 @@
+const myFunction = () => {
+    window.open("file:///media/fuse/drivefs-d3bbaa61bcc9f88f7792c111be2d650d/root/coding/game2/game%202.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=800,width=600,height=990");
+}
